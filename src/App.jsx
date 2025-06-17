ab7c256bb31812d029a44fd54326e3f8253e3e65
Robert Glaser <robertbglaser@gmail.com>
2025-06-17 16:24:48 -0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import Info from './components/Info'
+import Details from './components/Details'
+import Other from './components/Other'
+import Endings from './components/Endings'
+import Footer from './components/Footer'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <Info/>
+     <Details/>
+     <Other/>
+     <Endings/>
+
+     <Footer/>
+        
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Info.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Info = () => {
+  return (
+    <> 
+    <form> 
+   <label>
+    What is your name?*
+     </label><br/>
+    <input type='text'/>
+  
+<br/>
+    <label>
+    Are you able to attend*</label><br/>
+    <button>Yes</button><br/>
+    <button>No</button>
+   
+<br/>
+<br/>
+   <button>Next</button>
+   </form>
+   </>
+  )
+}
+
+export default Info
