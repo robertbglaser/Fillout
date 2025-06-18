7be626bdf73f8e2974a1472e6fae9643ac84a450
Robert Glaser <robertbglaser@gmail.com>
2025-06-18 15:11:17 -0700

    Context menu is working. saving code

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,48 @@
 import React from 'react'
 import { Button, ButtonGroup, FormControl, InputLabel,Input } from '@mui/material';
-import ContextMenu from './ContextMenu';
-import { createIcons, icons } from 'lucide';
-import { Check, EllipsisVertical, Plus, NotebookText } from 'lucide-react';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import Typography from '@mui/material/Typography';
+import { DndContext } from '@dnd-kit/core';
+import { Check, EllipsisVertical, Plus, NotebookText, Flag, Pencil,Clipboard, Copy, Delete } from 'lucide-react';
 
-createIcons({ icons });
+
 
   
-
+    
 const Footer = () => {
+      const [contextMenu, setContextMenu] = React.useState(null);
+      
+    const handleContextMenu = (event) => {
+      event.preventDefault();
+  
+      setContextMenu(
+        contextMenu === null
+          ? {
+              mouseX: event.clientX + 2,
+              mouseY: event.clientY - 6,
+            }
+          : // repeated contextmenu when it is already open closes it with Chrome 84 on Ubuntu
+            // Other native context menus might behave different.
+            // With this behavior we prevent contextmenu from the backdrop to re-locale existing context menus.
+            null,
+      );
+  
+      // Prevent text selection lost after opening the context menu on Safari and Firefox
+      const selection = document.getSelection();
+      if (selection && selection.rangeCount > 0) {
+        const range = selection.getRangeAt(0);
+  
+        setTimeout(() => {
+          selection.addRange(range);
+        });
+      }
+    };
+  
+    const handleClose = () => {
+      setContextMenu(null);
+    };
+
 
     const buttons=[
         {
@@ -38,11 +72,28 @@ const Footer = () => {
     ]
     
   return (
-    <div>
+    <div onContextMenu={handleContextMenu} style={{ cursor: 'context-menu' }}>
+        <Menu
+        open={contextMenu !== null}
+        onClose={handleClose}
+        anchorReference="anchorPosition"
+        anchorPosition={
+          contextMenu !== null
+            ? { top: contextMenu.mouseY, left: contextMenu.mouseX }
+            : undefined
+        }
+      >
+        <MenuItem onClick={handleClose}><Flag/>Set as First Page</MenuItem>
+        <MenuItem onClick={handleClose}><Pencil/>Rename</MenuItem>
+        <MenuItem onClick={handleClose}><Clipboard/>Copy</MenuItem>
+        <MenuItem onClick={handleClose}><Copy/>Duplicate</MenuItem>
+        <MenuItem onClick={handleClose}><hr/>------------</MenuItem>
+        <MenuItem onClick={handleClose}><Delete/> Delete</MenuItem>
+      </Menu>
         <ButtonGroup variant="outlined"> 
     {buttons.map((desc)=>{
         return(
-         <Button 
+         <Button
          > 
           {desc.icon}
           {desc.text}
