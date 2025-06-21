8e0912251be16b79886d9e228a3c5cf0693a5c1d
Robert Glaser <robertbglaser@gmail.com>
2025-06-20 17:17:18 -0700

    added checkboxes

--- src/components/AnythingElse.jsx
+++ src/components/AnythingElse.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { useNavigate } from 'react-router-dom';
 import { ArrowRight,  Asterisk, Check } from 'lucide-react';
 import TextareaAutosize from '@mui/material/TextareaAutosize';
-import { Container,Button, Radio, ButtonGroup, RadioGroup, InputLabel,Input, FormGroup, Typography } from '@mui/material';
+import { Container,Button, Radio, ButtonGroup, RadioGroup, InputLabel,Input, FormControlLabel,FormGroup, Typography, FormLabel, FormControl } from '@mui/material';
 
 const AnythingElse = () => {
      let navigate = useNavigate()
@@ -15,15 +15,17 @@ const AnythingElse = () => {
 <br/>
 <br/>
      
-           <Typography variant='h6' sx={{color:'#FFFF'}}>Anything else you'd like to let us know?
-           <br/>
+            
+           <FormControl>
+           <FormLabel sx={{color:'#FFFF'}}>Anything else you'd like to let us know?</FormLabel>
            <RadioGroup>
-    
-                     <Radio value="Yes" label="Yes" variant="solid"  orientation="horizontal"></Radio>
-                     <Radio value="No" label="No" variant="solid" sx={{color:'#ffffff '}} orientation="horizontal"></Radio>
+              <FormControlLabel value="Yes" control={<Radio/>} label={<Check/> } sx={{color:'#FFFF'}} variant="solid"color="danger"/>
+               <FormControlLabel value="No" control={<Radio/>} label={'X  NO'  } sx={{color:'#FFFF'}} variant="solid"color="danger"/>
+                    
                  
                  </RadioGroup>
-                 </Typography >
+                 </FormControl>
+                 {/* </Typography > */}
                  <br/>
                   <br/>
                 <TextareaAutosize minRows={5} variant="outlined" />
--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom';
 import { ArrowRight,  Asterisk, Check } from 'lucide-react';
-import {Box, Button, ButtonGroup, Input, RadioGroup,   Radio,TextField,Typography, Container} 
+import {Box, Button, ButtonGroup, Input, FormLabel, FormControl, FormControlLabel, RadioGroup,   Radio,TextField,Typography, Container} 
 from '@mui/material';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
@@ -31,9 +31,17 @@ const Info = () => {
     </Box>
      
 <br/>
-<br/>
+<br/> <FormControl>
+           <FormLabel sx={{color:'#FFFF'}}>Are you able to attend <Asterisk/></FormLabel>
+           <RadioGroup>
+              <FormControlLabel value="Yes" control={<Radio/>} label={<Check/> } sx={{color:'#FFFF'}} variant="solid"color="danger"/>
+               <FormControlLabel value="No" control={<Radio/>} label={'X  NO'  } sx={{color:'#FFFF'}} variant="solid"color="danger"/>
+                    
+                 
+                 </RadioGroup>
+                 </FormControl>
      
-           <Typography variant='h6' sx={{color:'#FFFF'}}>Are you able to attend?<Asterisk/>
+           {/* <Typography variant='h6' sx={{color:'#FFFF'}}>Are you able to attend?<Asterisk/>
            <br/>
             <RadioGroup defaultValue="outlined" name="radio-buttons-group">
                 <Radio value="Yes"  sx={{bgColor:'#000000', color:'#FFFF',borderColor:'lightgray'}} 
@@ -43,7 +51,7 @@ const Info = () => {
 
                 </Radio>
             </RadioGroup>
-           </Typography >
+           </Typography > */}
     
        
    <br/>
